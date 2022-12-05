acb761ac765bfa1df78716f7f1e2c1bcccf6b2bd
Maicon Silva Florencio <msflorencio@stefanini.com>
2022-12-05 11:41:02 -0300

    inserido swagger para aplicacao

--- src/main/java/com/learning/feign/soufan/SoufanApplication.java
+++ src/main/java/com/learning/feign/soufan/SoufanApplication.java
@@ -3,9 +3,13 @@ package com.learning.feign.soufan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableWebMvc
+@EnableSwagger2
 public class SoufanApplication {
 
 	public static void main(String[] args) {
