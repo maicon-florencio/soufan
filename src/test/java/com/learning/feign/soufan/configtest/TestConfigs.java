3cfa3bd793a97543bf97a1aa69e2aef8edc4e031
Maicon Silva Florencio <msflorencio@stefanini.com>
2022-12-07 10:41:45 -0300

    adicionar novas dependencia para conexao com banco

---
+++ src/test/java/com/learning/feign/soufan/configtest/TestConfigs.java
@@ -0,0 +1,6 @@
+package com.learning.feign.soufan.configtest;
+
+public class TestConfigs {
+
+    public static final int SERVER_PORT = 8888;
+}
---
+++ src/test/java/com/learning/feign/soufan/rest/integration/AbstractIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.learning.feign.soufan.rest.integration;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
+public class AbstractIntegrationTest {
+
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+       final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.29");
+
+        private  void startContainers() {
+            Startables.deepStart(Stream.of(mysql)).join();
+        }
+
+        private Map<String, String> createConnectionConfiguration() {
+            return Map.of(
+                    "spring.datasource.url", mysql.getJdbcUrl(),
+                    "spring.datasource.username", mysql.getUsername(),
+                    "spring.datasource.password", mysql.getPassword()
+            );
+        }
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            startContainers();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testcontainers = new MapPropertySource(
+                    "testcontainers",
+                    (Map) createConnectionConfiguration());
+            environment.getPropertySources().addFirst(testcontainers);
+        }
+    }
+}
